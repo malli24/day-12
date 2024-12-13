3297159cff235a4c82367719093fbd8523c2882d
malli24 <malleswari82006@gmail.com>
2024-12-13 12:33:33 +0530

    Add files via upload

---
+++ printingset.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class printingset
+{
+    public static void main(String[]args)
+{
+LinkedList<String>a=new LinkedList<>();
+a.add("Hello");
+a.add("Hello");
+a.add("malli");
+a.add("school");
+a.add("malli");
+for(String s:a)
+{
+ System.out.println(s+" ");
+}
+}
+}

9bed54b1766220205058294c2889f87aeac97c97
malli24 <malleswari82006@gmail.com>
2024-12-13 12:34:07 +0530

    Add files via upload

---
+++ setinterface.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class setinterface
+{
+    public static void main(String[]args)
+{
+LinkedList<String>a=new Linkedhashset<>();
+a.add("Hello");
+a.add("Hello");
+a.add("malli");
+a.add("school");
+a.add("malli");
+a.remove("malli");
+}
+}
